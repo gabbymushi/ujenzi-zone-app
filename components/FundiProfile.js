729a96aa7b54dc3b41143a89c78776a1d6433101
Gabriel Patrick <gabrielpatrickmushi@gmail.com>
2020-01-11 14:30:43 +0300

    update

--- components/FundiProfile.js
+++ components/FundiProfile.js
@@ -1,77 +1,51 @@
-//This is an example code for Navigation Drawer with Custom Side bar//
 import React, { Component } from 'react';
-//import react in our code.
-import { StyleSheet, View, Text, Button, TextInput, SafeAreaView, Image, ScrollView } from 'react-native';
-// import all basic components
-import Logo from './Logo';
+import { Text, View } from 'react-native';
+import {
+} from 'react-navigation';
+
+import { createMaterialTopTabNavigator } from 'react-navigation-tabs';
+import StickyTabs from '../sticky-tabs/StickyTabs';
+
+import Header from './Header';
+import Feed from './Feed';
+import Info from './Info';
+
+const TabNavigator = createMaterialTopTabNavigator(
+  {
+    feed: { screen: Feed },
+    info: { screen: Info },
+  },
+  {
+    tabBarPosition: 'top',
+    tabBarOptions: {
+      scrollEnabled: true,
+      activeTintColor: '#0084ff',
+      inactiveTintColor: '#626262',
+      labelStyle: {
+        fontWeight: 'bold',
+      },
+      indicatorStyle: {
+        backgroundColor: '#0084ff',
+      },
+      style: {
+        backgroundColor: 'white',
+      },
+    },
+  }
+);
 
 export default class FundiProfile extends Component {
-    //Screen2 Component
-    render() {
-        return (
-            <SafeAreaView style={styles.container}>
-                <ScrollView showsHorizontalScrollIndicator={false}>
-                    <View style={styles.titleBar}>
-                    </View>
-                    <View style={{ alignSelf: 'center' }}>
-                        <View style={styles.profileImage}>
-                            <Image source={require("../images/drawer.png")} style={styles.image}></Image>
-                        </View>
-                    </View>
-                    {/* <View style={styles.active}></View> */}
-                    <View style={styles.infoContainer}>
-                        <Text style={styles.text,{fontWeight:"200",fontSize:36}}>Gabriel Patrick </Text>
-                        <Text style={styles.text,{color:"#AEB5BC",fontSize:14}}>Fundi Mwashi</Text>
-                    </View>
-                </ScrollView>
-            </SafeAreaView>
-        );
-    }
-}
+  
+  static router = TabNavigator.router;
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        //paddingTop: 20,
-        alignItems: 'center',
-        //marginTop: 50,
-        justifyContent: 'center',
-    },
-    text: {
-        fontFamily: 'HelveticaNeue',
-        color: '#52575D'
-    },
-    image: {
-        flex: 1,
-        width: undefined,
-        height: undefined
-    },
-    profileImage: {
-        width: 150,
-        height: 150,
-        borderRadius: 100,
-        overflow:"hidden"
-    },
-    titleBar: {
-        flexDirection: "row",
-        justifyContent: "space-between",
-        marginTop: 24,
-        marginHorizontal:16
-    },
-    active: {
-        backgroundColor: "#34FFB9",
-        position: "absolute",
-        bottom: 28,
-        left:10,
-        padding:10,
-        height:20,
-        width:20,
-        borderRadius:10,
-    },
-    infoContainer: {
-     alignSelf:"center",
-     alignItems:"center",
-     marginTop:16
-    }
-});
+  render() {
+    return (
+      <StickyTabs
+        headerHeight={150}
+        headerComponent={<Header />}
+        contentComponent={<TabNavigator navigation={this.props.navigation} />}
+      />
+    );
+  }
+  
+}
